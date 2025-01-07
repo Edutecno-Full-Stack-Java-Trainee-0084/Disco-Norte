f8eac6e091e3dffa127e0d58969b374dda57222b
AlonsoMeza-X <a.meza.ercoli@gmail.com>
2025-01-06 23:07:20 -0300

    actualizacion 6.3.2

--- src/main/java/cl/playground/disco_norte/controller/MemberPurchaseRestController.java
+++ src/main/java/cl/playground/disco_norte/controller/MemberPurchaseRestController.java
@@ -1,5 +1,6 @@
 package cl.playground.disco_norte.controller;
 
+import cl.playground.disco_norte.dto.MemberPurchaseDetailsResponseDTO;
 import cl.playground.disco_norte.dto.MemberPurchaseResponseDTO;
 import cl.playground.disco_norte.model.MemberPurchase;
 import cl.playground.disco_norte.service.MemberPurchaseService;
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/purchase")
+@RequestMapping("/api/purchases")
 public class MemberPurchaseRestController {
 
     private final MemberPurchaseService memberPurchaseService;
@@ -37,49 +39,16 @@ public class MemberPurchaseRestController {
 //        return memberPurchaseService.getAllMememberPurchases(pageable);
 //    }
 //
-//    @GetMapping("/{id}")
-//    public Page<MemberPurchase> getPurchaseById(@PathVariable Long id, Pageable pageable) {
-//        return memberPurchaseService.getMememberPurchasesByID(pageable, id);
-//    }
-/*
-    @GetMapping("/simplified")
-    public ResponseEntity<Map<String, Object>> getPurchasesSimplified(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
-
-        Page<MemberPurchase> purchasePage = memberPurchaseService.getAllMemberPurchases(
-                PageRequest.of(page, size)
-        );
-
-        return ResponseEntity.ok(createPaginationResponse(purchasePage));
-    }
-
-
     @GetMapping("/{id}")
-    public ResponseEntity<MemberPurchase> getMemberPurchaseDetails(
-            @PathVariable Long id,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
-        MemberPurchase memberPurchases = memberPurchaseService.getMemberPurchasesByID(id);
-        if (memberPurchases == null) {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<MemberPurchaseDetailsResponseDTO> getPurchaseById(@PathVariable Long id) {
+        Optional<MemberPurchaseDetailsResponseDTO> purchase = memberPurchaseService.getPurchase(id);
+
+        // Map<String, Object> errors....
+        if (purchase.isPresent()) {
+            return ResponseEntity.ok(purchase.get());
         }
-        return ResponseEntity.ok(memberPurchases);
-    }
 
-
-    private Map<String, Object> createPaginationResponse(Page<?> page) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("content", page.getContent());
-        response.put("currentPage", page.getNumber());
-        response.put("totalItems", page.getTotalElements());
-        response.put("totalPages", page.getTotalPages());
-        response.put("size", page.getSize());
-        response.put("hasNext", page.hasNext());
-        response.put("hasPrevious", page.hasPrevious());
-        return response;
+        return ResponseEntity.notFound().build();
     }
-    */
 
 }
